e46c92da67f307113614c4ca796f4b1340ecfc70
chrisgonvill <124818047+chrisgonvill@users.noreply.github.com>
2023-09-26 02:55:17 +0000

    generator

---
+++ app.js
@@ -0,0 +1,18 @@
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let dom = [".com", ".net", ".us", ".io"];
+
+var counter = 0;
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let l = 0; l < dom.length; l++) {
+        let domain = pronoun[i] + adj[j] + noun[k] + dom[l];
+        console.log(domain);
+        counter = counter + 1;
+      }
+    }
+  }
+}
+console.log(counter);
